fd508c8440e61d144d7d519faf100bdc308a4dfb
juan9572 <jprinconu@eafit.edu.co>
2022-08-09 08:16:33 -0500

    First problems of Warmup-1

---
+++ Warmup-1/diff21.cpp
@@ -0,0 +1,15 @@
+class Solution{
+    public:
+    /*
+    Given an int n, return the absolute difference between n and 21,
+    except return double the absolute difference if n is over 21.
+
+
+    diff21(19) → 2
+    diff21(10) → 11
+    diff21(21) → 0
+    */
+    int diff21(int n) {
+        return n>21?(n-21)*2:21-n;
+    }
+};
---
+++ Warmup-1/sumDouble.cpp
@@ -0,0 +1,15 @@
+class Solution{
+    public:
+    /*
+    Given two int values, return their sum.
+    Unless the two values are the same,then return double their sum.
+
+
+    sumDouble(1, 2) → 3
+    sumDouble(3, 2) → 5
+    sumDouble(2, 2) → 8
+    */
+    int sumDouble(int a, int b) {
+        return a==b?(a+b)*2:a+b;
+    }
+};
